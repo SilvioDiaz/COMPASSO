018c5de0b312fbd12e6d877e2ea2b45be6fa0f09
SilvioDiaz <57494579+SilvioDiaz@users.noreply.github.com>
2021-06-27 22:20:47 -0300

    Melhoria de Indentação

--- src/hooks/useGetUser.js
+++ src/hooks/useGetUser.js
@@ -1,24 +1,24 @@
-import {useState} from "react"
+import { useState } from "react"
 import axios from "axios"
-import { BASE_URL,headers } from "../constant/url"
+import { BASE_URL, headers } from "../constant/url"
 
 const useGetUser = () => {
-    
-const [profile,setProfile] = useState({})
 
-//Recebe os dados de usuario do GitHub
-const getUser = (username) => {
-    axios
-        .get(`${BASE_URL}/${username}`, headers)
-        .then((response) => {
-            setProfile(response.data)
-        })
-        .catch((err) => {
-            setProfile({})
-        })
-}
+    const [profile, setProfile] = useState({})
 
-return {getUser,profile}
+    //Recebe os dados de usuario do GitHub
+    const getUser = (username) => {
+        axios
+            .get(`${BASE_URL}/${username}`, headers)
+            .then((response) => {
+                setProfile(response.data)
+            })
+            .catch((err) => {
+                setProfile({})
+            })
+    }
+
+    return { getUser, profile }
 }
 
 export default useGetUser
